f4c6c7e573ed9b5829f81577ccf5bc84e10d3e58
DanilSereda22 <seredadanil944@gmail.com>
2025-11-30 14:21:35 +0300

    Красивый интерфейс. Задачи: доработать корзину добавления,выбор самовызов,курьер.Регистрация не работает.

--- store/urls.py
+++ store/urls.py
@@ -6,13 +6,7 @@ app_name = "store"
 
 urlpatterns = [
     path("", views.product_list, name="product_list"),
-
-    # Категории
     path("category/<slug:slug>/", views.category_view, name="category"),
-
-    # Подкатегории
     path("subcategory/<slug:slug>/", views.subcategory_view, name="subcategory"),
-
-    # Товар
     path("product/<slug:slug>/", views.product_detail, name="product_detail"),
 ]
